dc23ef58f1162099d78957c24e5ab7dd3fe97e99
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-02-22 16:49:22 -0600

    adjustments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://http://localhost:3000/'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Shots & Reels</h1>
   );
 }
 
